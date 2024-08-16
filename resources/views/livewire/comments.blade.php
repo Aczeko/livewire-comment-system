8c888099efe2d25d471f85d2fb43196f1f97e966
Aczeko <amina.cajlakovic@stud.edubs.ch>
2024-08-16 11:49:33 +0200

    add create and save comments function

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,3 +1,17 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <h1>
+        Comments (Comments Count)
+    </h1>
+
+    @auth
+        <form wire:submit="createComment" class="mt-4">
+            <div>
+                <x-textarea placeholder="Post a comment" class="w-full" rows="4" wire:model="form.body"/>
+                <x-input-error :messages="$errors->get('form.body')"/>
+            </div>
+            <x-primary-button class="mt-2">
+                Post a comment
+            </x-primary-button>
+        </form>
+    @endauth
 </div>
