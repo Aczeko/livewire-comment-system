c63e21cf7ee9297fbcc6ce6d4a8c68d62ecd41c4
Aczeko <amina.cajlakovic@stud.edubs.ch>
2024-08-16 11:27:27 +0200

    create polymorphic comments

--- resources/views/livewire/article-show.blade.php
+++ resources/views/livewire/article-show.blade.php
@@ -4,7 +4,7 @@
     </h2>
 </x-slot>
 
-<div class="py-12 space-y-6">
+<div class="py-12 space-y-4">
     <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -16,7 +16,7 @@
     <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
-                Comments
+                <livewire:comments :model="$article" />
             </div>
         </div>
     </div>
